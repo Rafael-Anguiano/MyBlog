884d9b1ffd34363a0546a8f05103d776019d39c0
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-02-17 15:38:20 -0600

    Header mobile view

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -49,10 +49,10 @@ const Post = ({post, setPage}) => {
                 <Link to={`/post/${post.id}`}><h2 onClick={handleClick}>{post.title}</h2></Link>
                 <p className='description' dangerouslySetInnerHTML={{__html: description}} ></p>
                 <div className='social-media'>
-                <a href={'https://www.linkedin.com/in/rafael-anguiano/'} target="_blank" rel="noreferrer"><button><FaLinkedin/></button></a>
-                <a href={'mailto:rj.anguiano.23@gmail.com'} target="_blank" rel="noreferrer"><button><MdEmail/></button></a>
-                <a href={'https://wa.me/+523331504871'} target="_blank" rel="noreferrer"><button><FaWhatsapp/></button></a>
-                <a href={'https://twitter.com/Rezuks23'} target="_blank" rel="noreferrer"><button><FaTwitter/></button></a>
+                    <a href={'https://www.linkedin.com/in/rafael-anguiano/'} target="_blank" rel="noreferrer"><button><FaLinkedin/></button></a>
+                    <a href={'mailto:rj.anguiano.23@gmail.com'} target="_blank" rel="noreferrer"><button><MdEmail/></button></a>
+                    <a href={'https://wa.me/+523331504871'} target="_blank" rel="noreferrer"><button><FaWhatsapp/></button></a>
+                    <a href={'https://twitter.com/Rezuks23'} target="_blank" rel="noreferrer"><button><FaTwitter/></button></a>
                 </div>
             </div>
         </article>
