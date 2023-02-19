027494cd922b533badc2d874f89d72d579c33eef
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-02-18 20:49:32 -0600

    Removing social links from header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { FaTwitter, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
 
 const Header = () => {
   let navigate = useNavigate()
@@ -14,12 +12,6 @@ const Header = () => {
           <li className='contact-btn'><button onClick={() => navigate('/contact')}>Contact me</button></li>
         </ul>
       </nav>
-      <div className='social-links'>
-        <a href={'https://www.linkedin.com/in/rafael-anguiano/'} target="_blank" rel="noreferrer"><button><FaLinkedin/></button></a>
-        <a href={'mailto:rj.anguiano.23@gmail.com'} target="_blank" rel="noreferrer"><button><MdEmail/></button></a>
-        <a href={'https://wa.me/+523331504871'} target="_blank" rel="noreferrer"><button><FaWhatsapp/></button></a>
-        <a href={'https://twitter.com/Rezuks23'} target="_blank" rel="noreferrer"><button><FaTwitter/></button></a>
-      </div>
     </header>
   )
 }
