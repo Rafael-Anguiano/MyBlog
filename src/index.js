6ef94c668f4e768b1d509f25ff9e2e0b97251c65
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-02-19 01:02:30 -0600

    Removing for now prerendering

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ import React from 'react';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 
-const SNAP = (
+// const SNAP = (
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App/>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// )
+
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(SNAP, rootElement);
+// } else {
+//   render(SNAP, rootElement);
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
   </React.StrictMode>
-)
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(SNAP, rootElement);
-} else {
-  render(SNAP, rootElement);
-}
+);
