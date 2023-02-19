643d8dce764ecaa3b9a425c3947c467c7fa6bf75
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-02-18 21:14:29 -0600

    Helmet

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import About from './About'
 import {FaRegComment} from 'react-icons/fa'
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 
 const API_KEY = process.env.REACT_APP_API_KEY
 const BLOG_ID = process.env.REACT_APP_BLOG_ID
-
 
 let defPost = {
     "published": "",
@@ -25,9 +25,21 @@ let defPost = {
 
 const Page = ({page = defPost}) => {
 
+    const getImage = (content) => {
+        const searchTerm = 'src=';
+        const searchEnd = '"'
+        const startOfSource = content.indexOf(searchTerm)+5;
+        let image = content.slice(startOfSource)
+        const endOfSource = image.indexOf(searchEnd);
+        image = image.slice(0, endOfSource)
+        return `${image}`
+    }
+
+    
     let { id } = useParams()
     const [post, setPost] = useState(page)
-
+    const location = useLocation();
+    let image = getImage(post?.content)
 
     useEffect(() => {
         if(page === defPost){
@@ -40,18 +52,26 @@ const Page = ({page = defPost}) => {
 
     return (
         <div className='columns-page'>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{post.title}</title>
+                <meta property="og:title" content={post.title}/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:image" content={image}/>
+                <meta property="og:url" content={`https://blog-rafael-anguiano.vercel.app${location.pathname}`}></meta>
+            </Helmet>
             <section className='page'>
-                <h2>{post.title}</h2>
+                <h2>{post?.title}</h2>
                 <div className='details'>
-                    <img alt='' className='autor' src={post.author.image.url}/>
-                    <small>{post.author.displayName}</small>
+                    <img alt='' className='autor' src={post?.author.image.url}/>
+                    <small>{post?.author.displayName}</small>
                     <div className='dot'></div>
-                    <small>{post.published.substring(0, 10)}</small>
+                    <small>{post?.published.substring(0, 10)}</small>
                     <div className='dot'></div>
                     <FaRegComment/>
-                    <small>{post.replies.totalItems}</small>
+                    <small>{post?.replies.totalItems}</small>
                 </div>
-                <div dangerouslySetInnerHTML={{__html: post.content}} className='content'/>
+                <div dangerouslySetInnerHTML={{__html: post?.content}} className='content'/>
             </section>
             <About/>
         </div>
