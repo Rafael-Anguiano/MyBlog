e9aa5b80cf6840808c6656230a4ea1e7bc6beb9d
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-02-21 13:40:06 -0600

    Removing comments

--- src/index.js
+++ src/index.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
-// import { hydrate, render } from "react-dom";
-
-// const SNAP = (
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App/>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// )
-
-// const rootElement = document.getElementById("root");
-// if (rootElement.hasChildNodes()) {
-//   hydrate(SNAP, rootElement);
-// } else {
-//   render(SNAP, rootElement);
-// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
