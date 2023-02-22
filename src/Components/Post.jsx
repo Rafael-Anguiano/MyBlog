e6095edaf3bd67e38bf92c50d8e91751f47413f2
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-02-22 15:39:29 -0600

    Social links to share content

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -8,7 +8,6 @@ const Post = ({post, setPage}) => {
 
     const image = getImage(post.content)
     const description = getDescription(post.content)
-    
     const handleClick = () => {
         setPage(post)
     }
@@ -29,10 +28,18 @@ const Post = ({post, setPage}) => {
                 <Link to={`/post/${post.id}`}><h2 onClick={handleClick}>{post.title}</h2></Link>
                 <p className='description' dangerouslySetInnerHTML={{__html: description}} ></p>
                 <div className='social-media'>
-                    <a href={'https://www.linkedin.com/in/rafael-anguiano/'} target="_blank" rel="noreferrer"><button><FaLinkedin/></button></a>
-                    <a href={'mailto:rj.anguiano.23@gmail.com'} target="_blank" rel="noreferrer"><button><MdEmail/></button></a>
-                    <a href={'https://wa.me/+523331504871'} target="_blank" rel="noreferrer"><button><FaWhatsapp/></button></a>
-                    <a href={'https://twitter.com/Rezuks23'} target="_blank" rel="noreferrer"><button><FaTwitter/></button></a>
+                    <a href={`https://www.linkedin.com/sharing/share-offsite/?url=blog-rafael-anguiano.vercel.app/post/${post.id}`} target="_blank" rel="noreferrer">
+                        <button><FaLinkedin/></button>
+                    </a>
+                    <a href={`mailto:rj.anguiano.23@gmail.com?subject=${encodeURI("Rafael Anguiano | Blog")}&body=${encodeURI(`Please check this amazing post titled "${post.title}" in the next URL:`)+`%0Ahttps://blog-rafael-anguiano.vercel.app/post/${post.id} %0A`}`} target="_blank" rel="noreferrer">
+                        <button><MdEmail/></button>
+                    </a>
+                    <a href={`https://wa.me/+523331504871?text=${encodeURI(`Please check this amazing post titled '${post.title}' in the next URL:`)+`%0Ahttps://blog-rafael-anguiano.vercel.app/post/${post.id} %0A`}`} target="_blank" rel="noreferrer">
+                        <button><FaWhatsapp/></button>
+                    </a>
+                    <a href={`https://twitter.com/intent/tweet?text=${encodeURI(`Hey! Check out this Interesting Post:`)+`%0Ahttps://blog-rafael-anguiano.vercel.app/post/${post.id} %0A`}`} target="_blank" rel="noreferrer">
+                        <button><FaTwitter/></button>
+                    </a>
                 </div>
             </div>
         </article>
