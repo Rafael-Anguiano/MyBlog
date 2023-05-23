b08ac25f574bb5c066ee49473f67332a9927c194
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-05-23 01:33:15 -0600

    Changing Layout head component

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,19 @@
 import ColumnTwo from './components/ColumnTwo'
 import Navigation from './components/Navigation'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
-      <head>
+      <Head>
         <title>Rafael Anguiano | Blog</title>
-        <meta property='og:title' content='Rafael Anguiano | Blog' />
-        <meta property='og:type' content='website' />
-        <meta property='og:image' content='/images/image.png' />
-        <meta property='og:url' content='https://blog-rafael-anguiano.vercel.app/' />
-        <meta property='og:description' content='Hi All :) Check out my blog' />
-      </head>
+        <meta property='og:title' content='Rafael Anguiano | Blog' key='title' />
+        <meta property='og:type' content='website' key='type' />
+        <meta property='og:image' content='/images/image.png' key='image' />
+        <meta property='og:url' content='https://blog-rafael-anguiano.vercel.app/' key='url' />
+        <meta property='og:description' content='Hi All :) Check out my blog' key='description' />
+      </Head>
       <body>
         <main>
           <Navigation />
