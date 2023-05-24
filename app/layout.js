df1b3e43a294e6c6f0cfc31c9fe63806443f5e20
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-05-23 22:18:48 -0600

    Changing metatag for principal image

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,6 @@ import Navigation from './components/Navigation'
 import '../styles/globals.css'
 import Head from 'next/head'
 
-// export const metadata = {
-//   title: 'Rafael Anguiano | Blog',
-//   description: 'Hi Everyone :) Check out my blog',
-//   url: 'https://blog-rafael-anguiano.vercel.app/',
-//   type: 'website'
-// }
-
 export async function generateMetadata ({ params, searchParams }, parent) {
   return {
     title: 'Rafael Anguiano | Blog',
