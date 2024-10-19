b2705d36fe1909a83c73fe805a67c8c39e9f43fe
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-10-19 09:27:43 -0600

    Changed separation between posts

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,7 +1,7 @@
 'use server'
 import { signIn } from '@/auth'
 
-export async function authenticate (prevState, formData) {
+export async function authenticate (_currentState, formData) {
   try {
     await signIn('credentials', formData)
   } catch (error) {
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,33 @@
-import LoginForm from './LoginForm'
+'use client'
 
-const Page = async () => {
+import { authenticate } from '@/app/lib/actions'
+import { useFormState, useFormStatus } from 'react-dom'
+
+export default function Page () {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined)
+
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <form action={dispatch}>
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Password' required />
+      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <LoginButton />
+    </form>
   )
 }
 
-export default Page
+function LoginButton () {
+  const { pending } = useFormStatus()
+
+  const handleClick = (event) => {
+    if (pending) {
+      event.preventDefault()
+    }
+  }
+
+  return (
+    <button aria-disabled={pending} type='submit' onClick={handleClick}>
+      Login
+    </button>
+  )
+}
